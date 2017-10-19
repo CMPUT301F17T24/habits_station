47c4806b41460e80321ef75689c49aac4a2476da
yfeng3 <yfeng3@ualberta.ca>
2017-10-18 23:40:25 -0600

    update Ui design with andorid studio layout

---
+++ habit_station_program/app/src/main/java/cmput301/habit_station_program/SignIn.java
@@ -0,0 +1,13 @@
+package cmput301.habit_station_program;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
