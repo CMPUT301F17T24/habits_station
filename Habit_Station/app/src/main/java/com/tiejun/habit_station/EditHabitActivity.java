0f72bd42f6e672828d377e58c82f8aa694e6fbd8
yfeng3 <yfeng3@ualberta.ca>
2017-10-31 10:42:05 -0600

    update edit habit

--- Habit_Station/app/src/main/java/com/tiejun/habit_station/EditHabitActivity.java
+++ Habit_Station/app/src/main/java/com/tiejun/habit_station/EditHabitActivity.java
@@ -12,6 +12,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.R.attr.format;
+
 public class EditHabitActivity extends AppCompatActivity {
 
     @Override
@@ -19,8 +25,9 @@ public class EditHabitActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_habit);
 
-        // my code starts here
-
+        /*
+        set a click listener for edit title
+         */
         final EditText editTitle = (EditText) findViewById(R.id.editTitle);
         editTitle.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -30,6 +37,40 @@ public class EditHabitActivity extends AppCompatActivity {
 
             }
         });
+
+        /*
+        set a click listener for edit date
+         */
+        final EditText editDate = (EditText) findViewById(R.id.editDate);
+        editDate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+
+                try {
+                    SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+
+                    Date newDate = format.parse(editDate.getText().toString());// get the new title
+                }catch (ParseException ex){
+                    Date newDate = new Date(); // handle the exception by assign current date
+                }
+
+            }
+        });
+
+        /*
+        set a click listener for edit reason
+         */
+        final EditText editReason = (EditText) findViewById(R.id.editReason);
+        editReason.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+
+                String newReason = editReason.getText().toString();// get the new title
+
+            }
+        });
+
+
     }
     
 
