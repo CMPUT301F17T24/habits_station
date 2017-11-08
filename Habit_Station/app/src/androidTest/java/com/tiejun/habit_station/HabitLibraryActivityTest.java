4debc14b45d736f067aaefe0473e9749171cd147
Whiteologist <342395558@qq.com>
2017-11-08 16:17:19 -0700

    HabitLibraryActivityTest update
    testViewHabit() prototype(requires new code to run test)

--- Habit_Station/app/src/androidTest/java/com/tiejun/habit_station/HabitLibraryActivityTest.java
+++ Habit_Station/app/src/androidTest/java/com/tiejun/habit_station/HabitLibraryActivityTest.java
@@ -9,8 +9,15 @@ package com.tiejun.habit_station;
 
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.robotium.solo.Solo;
+
+import java.util.Calendar;
+import java.util.HashSet;
+
+import com.tiejun.habit_station.AddHabitActivity;
 
 /**
  * Created by CBB on 2017/11/7.
@@ -19,6 +26,10 @@ import com.robotium.solo.Solo;
 public class HabitLibraryActivityTest extends ActivityInstrumentationTestCase2 {
 
     private Solo solo;
+
+    public int set_year = 0;
+    public int set_month = 0;
+    public int set_day = 0;
 
     public HabitLibraryActivityTest() {
         super(com.tiejun.habit_station.HabitLibraryActivity.class);
@@ -36,10 +47,29 @@ public class HabitLibraryActivityTest extends ActivityInstrumentationTestCase2 {
         Activity activity = getActivity();
     }
 
+    public void testViewHabit() {
+        solo.assertCurrentActivity("Wrong Activity", HabitLibraryActivity.class);
+        ListView listview = (ListView) solo.getView(R.id.habits);
 
+        // if habit list is empty, add a new habit
+        if (listview.getCount() == 0) {
+            solo.clickOnButton("Add");
+            solo.assertCurrentActivity("Wrong Activity", AddHabitActivity.class);
+
+            // identical to AddHabitActivityTest
+            solo.enterText((EditText) solo.getView(R.id.title), "testHabit");
+            solo.enterText((EditText) solo.getView(R.id.reason), "testReason");
+            solo.clickOnButton("SAT");
+            solo.clickOnButton("SUN");
+            solo.clickOnButton("OK");
+            solo.assertCurrentActivity("Wrong Activity", HabitLibraryActivity.class);
+        }
+
+        solo.clickLongInList(0);
+    }
 
     public void testAddHabit() {
-        solo.assertCurrentActivity("Wrong Activity", HabitHistoryActivity.class);
+        solo.assertCurrentActivity("Wrong Activity", HabitLibraryActivity.class);
         solo.clickOnButton("Add");
         solo.assertCurrentActivity("Wrong Activity", AddHabitActivity.class);
     }
