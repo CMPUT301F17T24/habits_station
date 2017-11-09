64112d79c4fb6798ae479fc0702b06b87ac7d12d
YAOZHI LU <yaozhilu@YAOZHIdeMacBook-Pro.local>
2017-11-08 17:36:05 -0700

    changed icon

--- Habit_Station/app/build/generated/source/r/debug/com/tiejun/habit_station/R.java
+++ Habit_Station/app/build/generated/source/r/debug/com/tiejun/habit_station/R.java
@@ -3459,6 +3459,7 @@ containing a value of this type.
     public static final class mipmap {
         public static final int ic_launcher=0x7f030000;
         public static final int ic_launcher_round=0x7f030001;
+        public static final int icon=0x7f030002;
     }
     public static final class string {
         public static final int Comment=0x7f070015;
